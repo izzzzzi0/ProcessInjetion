945ce0e3cee6ede455798b1bc49db5ff30d062e7
DESKTOP-0CQUOKL\Administrator <dlwldud13467@naver.com>
2021-10-01 21:55:56 +0900

    add DLL Main

---
+++ dllmain.cpp
@@ -0,0 +1,33 @@
+﻿// dllmain.cpp : DLL 애플리케이션의 진입점을 정의합니다.
+#include "pch.h"
+#include <Windows.h>
+#include <stdio.h>
+
+DWORD WINAPI ShowMessage(LPVOID lParam) {
+    MessageBox(NULL, TEXT("You are stupid!!"), TEXT("MessageBox"), MB_OK);
+    return 0;
+}
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    HANDLE hThread = NULL;
+
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        OutputDebugString(L"myhack.dll Injection!!");
+        hThread = CreateThread(NULL, 0, ShowMessage, NULL, 0, NULL);
+        CloseHandle(hThread);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+
