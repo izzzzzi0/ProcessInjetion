d2d32a64bbc89c6d5c9baf590102fb6348f501b4
DESKTOP-0CQUOKL\Administrator <izzzzzi0@gmail.com>
2021-10-01 21:58:25 +0900

    add DLL injection

---
+++ Injection/DLL_Injection.cpp
@@ -0,0 +1,21 @@
+#include "windows.h"
+#include "tchar.h"
+#include "stdio.h"
+
+#define DEL_DLL_PATH ("C:\\Users\\Administrator\\source\\repos\\myhack\\Release\\myhack.dll")
+
+DWORD FindProcessID(LPCTSTR ProcessName);
+BOOL Inject(DWORD hwPID, LPCTSTR DllPath);
+
+
+int _tmain(int argc, TCHAR* argv[]) {
+	if (argc != 2) {
+		_tprintf(L"Please INPUT Process Name!!");
+		return 1;
+	}
+
+	DWORD dwPID = 0xFFFFFF;
+
+}
+
+
